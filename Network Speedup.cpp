cda3db52ad114aee8fe673cc196043142b98f220
souhhcong <41003815+souhhcong@users.noreply.github.com>
2019-09-17 09:59:21 +0700

    format lai

--- Network Speedup.cpp
+++ Network Speedup.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <queue>
+#include <vector>
 using namespace std;
 
 #define st first
@@ -11,7 +12,7 @@ struct q
 {
     int v;
     float d;
-int num;
+    int num;
     bool operator < (const q& o) const
     {
         return d > o.d;
@@ -21,7 +22,7 @@ int num;
 const int N = 1e5+5;
 const float INF = 1e9+9;
 int n, m, dev, u, v, w;
-float pow[11] = {1, 2, 4,8,16,32,64,128,256,512,1024};
+float pow[11] = {1,2,4,8,16,32,64,128,256,512,1024};
 vector<pair<int,int> > adj[N];
 float dist[N][20];
 priority_queue<q> pq;
@@ -44,14 +45,14 @@ signed main()
     while(!pq.empty())
     {
         int u = pq.top().v, num = pq.top().num;
-float d = pq.top().d;
+        float d = pq.top().d;
         pq.pop();
         if (dist[u][num] != d)
             continue;
         for (auto p: adj[u])
         {
             int v = p.st, w = p.nd;
-            for (int j = num; j <= dev; j++) // j-num la so may dat o canh noi <u,v>
+            for (int j = num; j <= dev; j++) // j-num la so may dat o canh <u,v>
             {
                 if (dist[u][num] + (1.0*w/pow[j-num]) < dist[v][j])
                 {
